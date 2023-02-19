68cb9c694351e996ab07fac12171548d6300014a
MikeRock51 <mikerockmusic51@gmail.com>
2023-02-19 19:15:30 +0100

    Low battery

--- drum_kit/index.js
+++ drum_kit/index.js
@@ -2,6 +2,8 @@ const drumBtn = document.querySelectorAll("button.drum");
 
 for (i = 0; i < drumBtn.length; i++) {
     drumBtn[i].addEventListener('click', function () {
-        alert("Won ti click me ooo");
+        // alert("Won ti click me ooo");
+        let tom = new Audio('./sounds/tom-1.mp3');
+        tom.play();
     });
 }
