1a707519bc2398ab48fe796b284824a3123ed039
MikeRock51 <mikerockmusic51@gmail.com>
2023-03-07 13:38:00 +0100

    Intro to node

---
+++ intro_to_node/README.md
@@ -0,0 +1,2 @@
+# Intro to Node
+Touch down! My first contact working with node js.
---
+++ intro_to_node/index.js
@@ -0,0 +1,5 @@
+const superheroes = require("superheroes");
+const supervillains = require("supervillains");
+
+console.log(superheroes.random() + " is a Super Hero");
+console.log(supervillains.random() + " is a Super Villain");
