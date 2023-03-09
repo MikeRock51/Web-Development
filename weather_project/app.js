64138045847178b78ea08db3130a924a2cfcc5c0
MikeRock51 <mikerockmusic51@gmail.com>
2023-03-09 17:26:18 +0100

    Add Weather Icon

--- weather_project/app.js
+++ weather_project/app.js
@@ -5,12 +5,20 @@ const https = require("https");
 const port = 3000;
 
 app.get('/', function(req, res) {
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Abuja,ng&appid=ef71cbbd570d83e95791c02b1c79715b&units=metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=Osun,ng&appid=ef71cbbd570d83e95791c02b1c79715b&units=metric";
     https.get(url, function(response) {
         console.log(response.statusCode);
         response.on("data", function(data) {
             weatherData = JSON.parse(data);
-            console.log(weatherData.weather[0].id);
+            const temperature = weatherData.main.temp;
+            const description = weatherData.weather[0].description;
+            const city = weatherData.name;
+            const icon = `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`;
+            console.log(icon);
+            res.write(`<p>The weather currently shows ${description}</p>`);
+            res.write(`<h1>The temperature in ${city} is ${temperature} degree celcius</h1>`);
+            res.write(`<img src=${icon} alt="Weather Icon">`)
+            res.send();
         });
     });
 });
