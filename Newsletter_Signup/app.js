837c78a5786c6d4a10a3c7f99dda0f54cd6b4bb0
MikeRock51 <mikerockmusic51@gmail.com>
2023-03-10 15:12:30 +0100

    Set up to make API request

--- Newsletter_Signup/app.js
+++ Newsletter_Signup/app.js
@@ -13,9 +13,38 @@ app.get("/", function(req, res) {
 });
 
 app.post("/", function(req, res) {
-    console.log(req.body);
+    const userInfo = req.body;
+    const list_id = "b7604a5780"
+    const user_email = userInfo.email;
+    const user_fname = userInfo.firstName;
+    const user_lname = userInfo.lastName;
+    const apiServer = ""
+    const url = "https://$API_SERVER.api.mailchimp.com/3.0/lists/$list_id/members"
+
+
+    const data = {
+        members: [
+            {
+                email_address: user_email,
+                status: "subscribed",
+                merge_fields: {
+                  FNAME: user_fname,
+                  LNAME: $user_lname
+                }
+              }
+        ]
+    }
+    const jsonData = JSON.stringify(data);
+
+
 });
 
 app.listen(port, function() {
     console.log(`Server is running on port ${port}`);
 })
+
+// API Key
+// 6f591ef8bce502c00a802fcd5f24d1d1-us9
+
+// Audience ID
+// b7604a5780
