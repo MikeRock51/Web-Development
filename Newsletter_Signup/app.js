2ad1b81890870b234bc655e36369eb412ed73319
MikeRock51 <mikerockmusic51@gmail.com>
2023-03-10 13:52:53 +0100

    Add POST method to form

--- Newsletter_Signup/app.js
+++ Newsletter_Signup/app.js
@@ -5,7 +5,16 @@ const port = 3000;
 
 const app = express();
 
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res) {
+    console.log(req.body);
+});
 
 app.listen(port, function() {
     console.log(`Server is running on port ${port}`);
