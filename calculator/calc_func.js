50e206dddece4cc4043d4e7ad8c8f8340219e7b1
MikeRock51 <mikerockmusic51@gmail.com>
2023-03-13 10:06:29 +0100

    Import calculator functions

--- calculator/calc_func.js
+++ calculator/calc_func.js
@@ -1,19 +1,19 @@
-function add(num1, num2) {
+exports.add = function (num1, num2) {
     return (num1 + num2);
 }
 
-function sub(num1, num2) {
+exports.sub = function (num1, num2) {
     return (num1 - num2);
 }
 
-function div(num1, num2) {
+exports.div = function(num1, num2) {
     return (num1 / num2);
 }
 
-function mul(num1, num2) {
+exports.mul = function (num1, num2) {
     return (num1 * num2);
 }
 
-function calculator(num1, num2, op) {
+exports.calculator = function (num1, num2, op) {
     return op(num1, num2);
 }
--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,32 +1,12 @@
-// import * as calc from __dirname + "/calc_func.js";
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const port = 3000;
 
+const calc = require(__dirname + "/calc_func");
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
-function add(num1, num2) {
-    return (num1 + num2);
-}
-
-function sub(num1, num2) {
-    return (num1 - num2);
-}
-
-function div(num1, num2) {
-    return (num1 / num2);
-}
-
-function mul(num1, num2) {
-    return (num1 * num2);
-}
-
-function calculator(num1, num2, op) {
-    return op(num1, num2);
-}
-
-// console.log(calculator(1, 2, add));
 
 app.listen(port, function () {
     console.log("Listening on port " + port);
@@ -43,16 +23,16 @@ app.post("/", function (req, res) {
 
     switch (op) {
         case "+":
-            res.send(`${num1} ${op} ${num2} = ${calculator(num1, num2, add)}`);
+            res.send(`${num1} ${op} ${num2} = ${calc.calculator(num1, num2, calc.add)}`);
             break;
         case "-":
-            res.send(`${num1} ${op} ${num2} = ${calculator(num1, num2, sub)}`);
+            res.send(`${num1} ${op} ${num2} = ${calc.calculator(num1, num2, calc.sub)}`);
             break;
         case "/":
-            res.send(`${num1} ${op} ${num2} = ${calculator(num1, num2, div)}`);
+            res.send(`${num1} ${op} ${num2} = ${calc.calculator(num1, num2, calc.div)}`);
             break;
         case "*":
-            res.send(`${num1} ${op} ${num2} = ${calculator(num1, num2, sub)}`);
+            res.send(`${num1} ${op} ${num2} = ${calc.calculator(num1, num2, calc.mul)}`);
             break;
         default:
             res.send("Unsupported operator");
