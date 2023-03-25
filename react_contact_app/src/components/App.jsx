ba005c3d69ea1c1cfdcd87001b0daf595a1c9330
MikeRock51 <mikerockmusic51@gmail.com>
2023-03-25 10:19:13 +0100

    Add emojipedia task

--- react_contact_app/src/components/App.jsx
+++ react_contact_app/src/components/App.jsx
@@ -2,8 +2,22 @@ import React from "react";
 import Card from "./Card";
 import contacts from "../contacts";
 
+
+function createCard(contact) {
+  return (
+    <Card
+      name={contact.name}
+      imgURL={contact.imgURL}
+      phone={contact.phone}
+      email={contact.email}
+    />
+  );
+}
+
 function App() {
   const contactList = [];
+
+  contacts.map(createCard)
 
   for (let i = 0; i < contacts.length; i++) {
     contactList.push(
@@ -12,7 +26,7 @@ function App() {
           name={contacts[i].name}
           imgURL={contacts[i].imgURL}
           phone={contacts[i].phone}
-          email={contacts[i].email}
+          email={contacts[i].email} 
         />
       </div>
     );
